1ebc9f110184bd5a704e8288355b25c8ea448068
Kei Sugimoto <kei.sugimoto@fusions.co.jp>
2023-12-29 23:56:49 +0900

    適用締日計算におけるswitch式を、締日指定方法クラスの多態で置き換える

--- src/table_driven_example_oop/期日指定方法.java
+++ src/table_driven_example_oop/期日指定方法.java
@@ -1,18 +1,27 @@
 package table_driven_example_oop;
 
 public enum 期日指定方法 {
-	即日,
-	月末日,
-	指定日付;
-
-	日付 適用締日(日付 取引日,Integer 指定日付) {
-		return switch (this) {
-			case 即日 -> 取引日;
-			case 月末日 -> 取引日.月末日();
-			case 指定日付 -> (取引日.日() <= 指定日付
+	即日{
+		@Override
+		日付 適用締日(日付 取引日, Integer 指定日付) {
+			return 取引日;
+		}
+	},
+	月末日{
+		@Override
+		日付 適用締日(日付 取引日, Integer 指定日付) {
+			return 取引日.月末日();
+		}
+	},
+	指定日付{
+		@Override
+		日付 適用締日(日付 取引日, Integer 指定日付) {
+			return  (取引日.日() <= 指定日付
 					? 取引日
 					: 取引日.ｎ期間後(1,期間単位.月))
 					.同月中指定日(指定日付);
-		};
-	}
+		}
+	};
+
+	abstract 日付 適用締日(日付 取引日,Integer 指定日付);
 }
